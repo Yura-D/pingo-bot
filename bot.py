32a1bcbea465528e82aff8a77ee3232a49480af4
Yura-D <dyachuk.yu@gmail.com>
2019-03-21 22:26:59 +0200

    Changed bot library and removed flask

--- bot.py
+++ bot.py
@@ -1,38 +1,89 @@
 import os
 import logging
-from aiogram import Bot, Dispatcher, executor, types
-
+from telegram.ext import Updater, CommandHandler
 
 try:
     TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
 except:
-    pass
-
-"""
-This is a echo bot.
-It echoes any incoming text messages.
-"""
+    raise Exception('Telegram api key do not exist')
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
-bot = Bot(token=TELEGRAM_API_KEY)
-dp = Dispatcher(bot)
-
-
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when client send `/start` or `/help` commands.
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+    )
+logger = logging.getLogger(__name__)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+def start(bot, update):
+    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
+
+def help_message(bot, update):
+    update.message.reply_text('Help message')
+
+
+def alarm(bot, job):
+    """Send the alarm message."""
+    bot.send_message(job.context, text='Ping!')
+
+
+def set_timer(bot, update, args, job_queue, chat_data):
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+
+        # Add job to queue
+        job = job_queue.run_once(alarm, due, context=chat_id)
+        chat_data['job'] = job
+
+        update.message.reply_text('Ping successfully set!')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+
+def unping(bot, update, chat_data):
+    """Remove the job if the user changed their mind."""
+    if 'job' not in chat_data:
+        update.message.reply_text('You have no active Pings')
+        return
+
+    job = chat_data['job']
+    job.schedule_removal()
+    del chat_data['job']
+
+    update.message.reply_text('Ping successfully unset!')
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Run bot."""
+    updater = Updater(TELEGRAM_API_KEY)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_message))
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("unping", unping, pass_chat_data=True))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    main()
+
