73dbd6507b622743cc5e73afc894a02d0e0eb1fe
Yura-D <dyachuk.yu@gmail.com>
2019-03-25 13:42:05 +0200

    Connected database, created tables and made storing data from user ping

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import os
 import logging
+from datetime import datetime
+
 from telegram.ext import Updater, CommandHandler
+from database import Message
+
 
 try:
     TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
@@ -18,6 +22,7 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     update.message.reply_text('Hi! Use /set <seconds> to set a timer')
 
+
 def help_message(bot, update):
     update.message.reply_text('Help message')
 
@@ -27,20 +32,41 @@ def alarm(bot, job):
     bot.send_message(job.context, text='Ping!')
 
 
-def set_timer(bot, update, args, job_queue, chat_data):
+def set_ping(bot, update, args, job_queue, chat_data):
     """Add a job to the queue."""
     chat_id = update.message.chat_id
     try:
         # args[0] should contain the time for the timer in seconds
-        due = int(args[0])
-        if due < 0:
+        interval = int(args[0])
+        if interval <= 0:
             update.message.reply_text('Sorry we can not go back to future!')
             return
+        
+        to_user = args[1]
+        if to_user[0] != '@':
+            update.message.reply_text('Please add user name ' +
+                                      'that starts with "@"!')
+        all_text = update.message.text
+        start_text = all_text.find(to_user) + len(to_user)
+        ping_text = all_text[start_text:].lstrip()
+
+        from_user = update.message.from_user
+        from_user_complite = (str(from_user.id) + ', ' +
+                             str(from_user.first_name) + ' ' +
+                             str(from_user.last_name))
+
+        Message.create(
+            chat_id=chat_id,
+            created=datetime.now(),
+            content=ping_text,
+            from_user=from_user_complite,
+            to_user=to_user
+        )
 
         # Add job to queue
-        job = job_queue.run_once(alarm, due, context=chat_id)
+        job = job_queue.run_repeating(alarm, interval, context=chat_id)
         chat_data['job'] = job
-
+        
         update.message.reply_text('Ping successfully set!')
 
     except (IndexError, ValueError):
@@ -72,11 +98,11 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_message))
-    dp.add_handler(CommandHandler("set", set_timer,
+    dp.add_handler(CommandHandler("ping_on", set_ping,
                                   pass_args=True,
                                   pass_job_queue=True,
                                   pass_chat_data=True))
-    dp.add_handler(CommandHandler("unping", unping, pass_chat_data=True))
+    dp.add_handler(CommandHandler("ping_off", unping, pass_chat_data=True))
 
     dp.add_error_handler(error)
 
---
+++ database.py
@@ -0,0 +1,29 @@
+import os
+
+from peewee import *
+from playhouse.db_url import connect
+
+
+try:
+    db = connect(os.environ.get('DATABASE_URL')) # add local sql database
+except:
+    raise Exception('Please add database variable to environment')
+
+
+class Message(Model):
+    reminder_id = PrimaryKeyField(null=False)
+    chat_id = CharField()
+    created = DateTimeField()
+    content = TextField()
+    from_user = CharField()
+    to_user = CharField()
+
+    class Meta:
+        database = db
+        db_table = 'messages'
+        ordering = ['-created']
+
+
+if __name__ == '__main__':
+    db.create_tables([Message])
+
